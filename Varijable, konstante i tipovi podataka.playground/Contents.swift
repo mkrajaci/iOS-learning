f1d066ac2221b6eca4348d743946440e10e48f9d
Mario Krajacic <krajacic.mario@gmail.com>
2017-11-25 15:58:04 +0100

    App finished

---
+++ Varijable, konstante i tipovi podataka.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+
+var mojeGodine : Int = 37
+
+let mojeIme : String = "Mario"
+
+let mojeGodineZaDesetGodina = mojeGodine + 10
+
+let mojeCijeloIme = mojeIme + " Krajacic"
